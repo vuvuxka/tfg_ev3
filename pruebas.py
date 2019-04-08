3efae6de955d6311349662e00a0721750ff3aaec
Eva Gala <vuvuxka@gmail.com>
2019-04-08 18:37:05 +0200

    Optimizado
    Creo que funciona pero tarda demasiado en ejecutarse el ciclo (como 4*dt).

--- pruebas.py
+++ pruebas.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 import time
 import sys
+import math
 from collections import deque
 import ev3dev.ev3 as ev3
 import parameters
 import importlib
 from ev3dev.core import GyroSensor
-from ev3dev2.motor import LargeMotor, OUTPUT_D, OUTPUT_A, SpeedDPS
+from ev3dev2.motor import LargeMotor, OUTPUT_D, OUTPUT_A, SpeedPercent
 import os
+from ev3dev.ev3 import *
+import statistics
 
 # CONSTANTES #
 diametro = 42 # (milimetros)
@@ -15,6 +18,9 @@ n_max = 7 # tamaño maximo del array de historico de posiciones
 Kp = 0.6
 Ki = 14
 Kd = 0.005
+error_d = 0
+error_0 = 0
+error_i = 0
 
 K_angle = 25
 K_rate = 1.3
@@ -23,70 +29,102 @@ K_vel = 75
 
 deg2rad = 0.0174533
 
+motorLeft  = LargeMotor(OUTPUT_D)
+motorRight = LargeMotor(OUTPUT_A)
+Gyro = open(ev3.GyroSensor()._path + "/value0", "rb")
+motorR = open(motorRight._path + "/position", "rb")
+motorL = open(motorRight._path + "/position", "rb")
+setMotorR = open(motorRight._path + "/duty_cycle_sp", "w")
+setMotorL = open(motorLeft._path + "/duty_cycle_sp", "w")
+
+
 def eprint(*args, **kwargs): # Funcion para sacar por terminal
     print(*args, file=sys.stderr, **kwargs)
+def FastRead(infile):
+    infile.seek(0)
+    return(int(infile.read().decode().strip()))
+def FastWrite(outfile,value):
+    outfile.truncate(0)
+    outfile.write(str(int(value)))
+    outfile.flush()
 
 def offset(): # Funcion para medir el offset inicial
-    Sound.tone(440,1000).wait()
-    m = 0
+    Sound.tone(440,100).wait()
+    time.sleep(0.1)
+    g = []
     for i in range(20):
-        m = m + Gyro.rate
-    m = m/20
+        g.append(FastRead(Gyro))
+        time.sleep(0.005)
+    m = statistics.mean(g)
     time.sleep(0.1)
-    Sound.tone(440,1000).wait()
+    Sound.tone(440,100).wait()
     time.sleep(0.1)
-    Sound.tone(440,1000).wait()
+    Sound.tone(440,100).wait()
     return m
 
-dt = (time.time() - 2)/2000 # (milisegundos)
+dt = (22 - 2)/2000 # (milisegundos)
 radio = diametro/(2*1000) # (metros)
 historico = [0] * n_max # (metros)
 pos_rel = 0
+eprint("dt = " + str(dt))
 
 nowError = False
 preError = False
 error_cont = 0
 error = 0
 
-Gyro = GyroSensor()
-Gyro.mode = 'GYRO-RATE'
 angle = 0 # (deg)
-rate = 0
-time.sleep(0.1)
+rate = 0 # (deg/s)
+time.sleep(0.01)
 media_angle = offset() # (deg)
 
-motorLeft  = LargeMotor(OUTPUT_D)
-motorRight = LargeMotor(OUTPUT_A)
+motorLeft.reset()
+motorRight.reset()
+motorLeft.duty_cycle_sp = 0
+motorRight.duty_cycle_sp = 0
+motorLeft.run_direct()
+motorRight.run_direct()
+
 vel = 0
 pos = 0
 avance = 0
 max_acel = 0
+extra_pwr = 0
 
+tiempo = time.time()
+t1 = []
+t2 = []
+t3 = []
 
 while True:
     pos_rel = pos_rel + vel*dt*0.002
-    val = (motorLeft.position + motorRight.position)/2 # (deg)
+    val = (FastRead(motorL) + FastRead(motorR))/2 # (deg)
     historico.append(val)
     val_ant = historico.pop()
     deg_vel = (val - val_ant)/n_max*dt
     vel = deg_vel*radio*deg2rad # (metros / s)
     pos = radio*val*deg2rad # (metros)
 
-    val = 0
-    for i in range(5):
-        val = val + Gyro.rate
-    val = val/5
+    t1.append(time.time()-tiempo)
+
+    val = statistics.mean([FastRead(Gyro) for i in range(5)])
+    t2.append(time.time()-tiempo)
+
     media_angle = media_angle*(1-dt*0.2) + val*dt*0.2
     rate = val - media_angle # (deg)
     angle = angle + rate*dt # (deg)
 
+
     ganancia = K_angle*angle + K_rate*rate + K_pos*(pos-pos_rel) + K_vel*vel
+    #eprint("k1*"+str(angle)+" + k2*"+str(rate)+" + k3*"+str(pos-pos_rel)+" + k4*"+str(vel)+" = "+str(ganancia))
 
     error_p = ganancia - pos_rel
-    error_d = error_d + error_t*dt
-    error_i = (error_p - error_0)/dt
+    error_i = error_i + error_p*dt
+    error_d = (error_0-error_p)/dt
+    error_0 = ganancia
 
     out = error_p*Kp + error_d*Kd + error_i*Ki
+    #eprint("kp*"+str(error_p)+" + kd*"+str(error_d)+" + ki*"+str(error_i)+" = "+str(out))
 
     nowError = (abs(out) > 100)
     if nowError & preError:
@@ -100,20 +138,45 @@ while True:
         Sound.tone(800,0.1)
         Sound.tone(600,0.1)
         Sound.tone(300,0.1)
-        wait(4)
+        time.sleep(4)
+        eprint("t1 = " + str(statistics.mean(t1)))
+        eprint("t2 = " + str(statistics.mean(t2)))
+        eprint("t3 = " + str(statistics.mean(t3)))
         exit()
     else:
         preError = nowError
 
     nuevo_avance = min(max(-50, avance), 50)
     if nuevo_avance == 0:
-        if avance == 0:
-            sync_0 = motorLeft.position -
+        if avance != 0:
+            sync_0 = FastRead(motorR) - FastRead(motorL)
+        else:
+            sync_0 = 0
+        extra_pwr = (FastRead(motorL) - FastRead(motorR) - sync_0)*0.05
+
+    #eprint("extra_pwr = " + str(extra_pwr))
+    power1 = out - extra_pwr
+    power2 = out + extra_pwr
+    avance = nuevo_avance
+    p2 = max(min((power2*0.021/radio), 100), -100)
+    p1 = max(min((power1*0.021/radio), 100), -100)
+    #eprint("powerA = " + str(p2))
+    #eprint("powerD = " + str(p1))
+    # motorRight.run_forever(speed_pc)
+    # motorLeft.run_forever(SpeedPercent(50))
+    FastWrite(setMotorR,p2)
+    FastWrite(setMotorL,p1)
+    now = time.time()
+    t3.append((now-tiempo))
+    #eprint("tiempo = " + str(now-tiempo))
+    while (now - tiempo) < dt:
+        time.sleep(0.001)
+        #eprint("Esperando...")
+    tiempo = time.time()
 
 
 
-
-
+"""
     time.sleep(0.002)
     rate = (rate + Gyro.rate)/2 - o
 
@@ -139,3 +202,4 @@ while True:
     motorRight.on(u)
     tloop = (time.time() - tstart) # Tiempo en hacer un bucle
 
+"""
